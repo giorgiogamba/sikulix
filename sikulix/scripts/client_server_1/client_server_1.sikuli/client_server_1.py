97cdc439a6f26cc868c8a74a1154553ae7130e4a
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-10-09 15:36:12 +0200

    refactor sikulix script and add text search inside region

--- sikulix/scripts/client_server_1/client_server_1.sikuli/client_server_1.py
+++ sikulix/scripts/client_server_1/client_server_1.sikuli/client_server_1.py
@@ -1,10 +1,15 @@
 # Activate OCR feature
+import subprocess
+import os
 import org.sikuli.basics.Settings as Settings
 Settings.OcrTextRead = True
 Settings.OcrTextSearch = True
 
-## Server screen setup
-server_screen = Screen(0) # set default screen
+# Looks for the client and server applications inside the screen and 
+# keeps track of them
+
+## Server screen
+server_screen = Screen(0)
 found_server = False
 
 for i in range(Screen.getNumberScreens()):
@@ -20,8 +25,8 @@ if not found_server:
     print("unable to find server. Exit...")
     exit()
 
-# Client screen setup    
-client_screen = Screen(0) # set default screen
+# Client screen setup
+client_screen = Screen(0)
 found_client = False
 
 for i in range(Screen.getNumberScreens()):
@@ -37,7 +42,7 @@ if not found_client:
     print("unable to find client. Exit...")
     exit()
 
-# #TEST: Put both applications in the top-left part of the screens,
+# #TEST: Put both applications on the top-left part of the screens,
 # in order to test the sikulix's Region search
 server_region = Region(server_screen.x, server_screen.y, 600, 600)
 server_region.highlight(1)
@@ -45,22 +50,46 @@ server_region.highlight(1)
 client_region = Region(client_screen.x, client_screen.y, 600, 600)
 client_region.highlight(1)
 
-# OCR feature test
+# OCR feature test 1
+# Read the provided image's text
 start_server_img = "1759398479349.png"
 img_path = os.path.join(getBundlePath(), start_server_img)
 
+server_text = ""
+
 try:
     # Direct Tesseract invokation for OCR
-    process = subprocess.Popen(
-        ['tesseract', img_path, 'stdout'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
+    process = subprocess.Popen(['tesseract', img_path, 'stdout'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
-    text = output.strip()
-    print("OCR Result:", text)
+    server_text = output.strip()
 except Exception as e:
     print("OCR Error:", e)
+    exit()
+
+if server_text == "":
+    print("Unable to find text, returning...")
+    exit()
+else:
+    print("OCR Result:", server_text)
+    
+# OCR feature test 2
+# Search for the text read from the image inside server region
+# NOTE: The search is not working when the button is in "Disabled" mode (the server is already running)
+server_region_path = capture(server_region)
+try:
+    process = subprocess.Popen(['tesseract', server_region_path, 'stdout'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    region_text = output.strip()
+    if server_text in region_text:
+        print("Found text inside server region")
+    else:
+        print("Text not found. Region contains:", region_text)
+        exit()
+except Exception as e:
+    print("OCR Error:", e)
+    exit()
+
+# TEST: Execute commands on client and check if working on server
 
 # Server start
 server_region.click("1759398479349.png")
@@ -70,7 +99,7 @@ server_region.wait("1759398529789.png")
 client_region.click("1759398719570.png")
 client_region.wait("1759398593383.png")
 
-# Command launch
+# Commands launch
 client_region.click("1759400416557.png")
 server_region.wait("1759400442376.png")
 client_region.click("1759400536979.png")
